0305363452b5a90d05573caaca97a9e2c549a254
Mayank <mak.2901@gmail.com>
2016-08-22 02:30:52 +0530

    first commit

--- client/routes.js
+++ client/routes.js
@@ -1,6 +1,7 @@
 var routes = {
 
 	"/": require('module/home/index'),
+	"/home": require('module/home/home'),
 	"/signup": require('module/home/connect'),
 	"/login": require('module/home/login'),
 	"/billing": require('module/billing')
--- client/src/home/home.jsx
+++ client/src/home/home.jsx
@@ -1,5 +1,7 @@
 var home={};
-
+$('.ui.dropdown')
+  .dropdown()
+;
 //ctrl
 home.controller=function(){
 	var ctrl={};
@@ -8,19 +10,71 @@ home.controller=function(){
 
 home.load=function(ctrl){
 	return (
-		<div class="container">
-			<div class="ui teal three item inverted menu">
-			  <a class="active item">
-			    Home
+			<div class="ui teal seven item inverted borderless menu">
+			  <a class="item">
+			    Vegetables
 			  </a>
 			  <a class="item">
-			    Messages
+			    Fruits
 			  </a>
-			  <a class="item">
-			    Friends
-			  </a>
+			  <div class="ui simple dropdown item">
+			  	Grocery
+			    <i class="dropdown icon"></i>
+			    <div class="menu">
+			      <a class="item">Pulses</a>
+			      <a class="item">Spices</a>
+			      <a class="item">Wheat & Flours</a>
+			      <a class="item">Oil & Ghee </a>
+			      <a class="item">Rice & Rice Products</a>
+			      <a class="item">Dry Fruits</a>
+			      <a class="item">Tea & Coffee</a>
+			      <a class="item">Salt & Sugar</a>
+			    </div>
+			  </div>
+			  <div class="ui simple dropdown item">
+			  	Packaged Foods
+			    <i class="dropdown icon"></i>
+			    <div class="menu">
+			      <a class="item">Biscuits</a>
+			      <a class="item">Noodles</a>
+			      <a class="item">Pickles</a>
+			      <a class="item">Snacks</a>
+			      <a class="item">Jams & Sauces</a>
+			      <a class="item">Ready To Eat</a>
+			    </div>
+			  </div>
+			  <div class="ui simple dropdown item">
+			  	Personal Care
+			    <i class="dropdown icon"></i>
+			    <div class="menu">
+			      <a class="item">Hair Care</a>
+			      <a class="item">Skin Care</a>
+			      <a class="item">Home Care</a>
+			      <a class="item">Digestive</a>
+			      <a class="item">Health Supplements</a>
+			    </div>
+			  </div>
+			   <div class="ui simple dropdown item">
+			  	Dairy Products
+			    <i class="dropdown icon"></i>
+			    <div class="menu">
+			      <a class="item">Breads</a>
+			      <a class="item">Milk</a>
+			      <a class="item">Paneer</a>
+			      <a class="item">Curd</a>
+			      <a class="item">Butter</a>
+			    </div>
+			  </div>
+			  <div class="ui simple dropdown item">
+			  	Beverages
+			    <i class="dropdown icon"></i>
+			    <div class="menu">
+			      <a class="item">Juices</a>
+			      <a class="item">Soft Drinks</a>
+			      <a class="item">Health Drinks</a>
+			    </div>
+			  </div>
 			</div>
-		</div>
 	)
 }
 
